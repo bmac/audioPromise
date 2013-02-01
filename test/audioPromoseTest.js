890a9350a4006088a1b2fde4c9c2bbed3cc3a362
bmac <bmac325@gmail.com>
2013-01-31 19:30:03 -0500

    added test for resolving the promise when the loaddata event has already fired

--- test/audioPromoseTest.js
+++ test/audioPromoseTest.js
@@ -1,5 +1,6 @@
+var audioUrl = 'audio/the_black_atlantic_-_dandelion.mp3';
 asyncTest( " returns a promise object which should resolve with the audio object", 2, function() {
-    var promise = $.audioPromise(new Audio('audio/the_black_atlantic_-_dandelion.mp3'));
+    var promise = $.audioPromise(new Audio(audioUrl));
     equal(typeof promise.done, 'function', 'typeof promise.done should be a function');
 
     promise.done(function(audio) {
@@ -15,5 +16,18 @@ asyncTest( "rejecte the promise if there is an error with the audio file", 1, fu
     promise.fail(function(audio) {
 	ok(true);
 	start();
+    });
+});
+
+asyncTest( "The promise should resolve if the loaddata event has alread fired", 1, function() {
+    var promise = $.audioPromise(new Audio(audioUrl));
+
+    promise.done(function(audio) {
+	var newPromise = $.audioPromise(audio);
+
+	newPromise.done(function() {
+	    ok(true);
+	    start();
+	});
     });
 });
